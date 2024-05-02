3a7d89b54be3d77d1d7190d06efe459fd0e4f240
Hanne Maes <maes.hanne@gmail.com>
2024-05-02 11:30:32 +0200

    Push fast: 2024-05-02 11:30:32

--- task-serve-windows.sh
+++ task-serve-windows.sh
@@ -2,4 +2,4 @@
 
 cd docs/
 start firefox http://127.0.0.1:4000/meneermaes/ # I first need to start the browser because the next command (serve) doenst stop
-bundle exec jekyll serve
+bundle exec jekyll serve --baseurl "" # --baseurl "" was added because when linking my meneermaes.be domain I had to change to baseurl to "" in _config.yml, and now it only works locally when I add --baseurl ""
