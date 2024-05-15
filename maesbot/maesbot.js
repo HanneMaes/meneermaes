8de2d2ed65941460b38eefbccd6a4b2291289d9f
Hanne Maes <maes.hane@gmail.com>
2024-05-15 21:07:01 +0200

    Push fast: 2024-05-15 21:07:01

--- maesbot/maesbot.js
+++ maesbot/maesbot.js
@@ -1,69 +1,93 @@
-const readline = require('readline');       // ask questions
+const readline = require('readline');		// ask questions
 const { spawn } = require('child_process'); // execute other scripts
+const fs = require('fs');					// access to filesystem
 
-/* *************** */
-/* INTRO QUESTIONS */
-/* *************** */
+/* ****************** */
+/* READ SETTINGS JSON */
+/* ****************** */
 
-console.log();
-console.log('1. Diff files?');
-console.log('2. Punten sheet maken?');
-console.log();
+try {
+	const data = fs.readFileSync('settings-maesbot.json', 'utf8'); 	// Read the contents of the JSON file synchronously
+	const jsonData = JSON.parse(data); 								// Parse the JSON data into a JavaScript object
 
-/* ************** */
-/* ASK WHAT TO DO */
-/* ************** */
+	// Get settings
+	const privateDataDir = jsonData['private data dir'];
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-rl.question('🤖 What do you want to do? ', (answer) => {
+	console.log();
+	console.log('settings-maesbot.json:');
+	console.log('- Private data directory:', privateDataDir);
+	
+	/* *************** */
+	/* INTRO QUESTIONS */
+	/* *************** */
+
+	console.log();
+	console.log('1. Diff files?');
+	console.log('2. Punten sheet maken?');
 	console.log();
 
-	// execute what you chose
-	if (answer == '1') {
+	/* ************** */
+	/* ASK WHAT TO DO */
+	/* ************** */
 
-		/* ********** */
-		/* DIFF FILES */
-		/* ********** */
+	const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+	});
+	rl.question('🤖 What do you want to do? ', (answer) => {
+		console.log();
 
-		console.log('🤖 Diff files:');
+		// execute what you chose
+		if (answer == '1') {
 
-		const pythonProcess = spawn('/usr/bin/python3', ['batchFileDiff.py'], { cwd: 'diff-file/' });
-		// /usr/bin/python3: the location of the Python3 binary, find with this command $ which python3
-		// cwd: the location from where the python scripts is executed
-		pythonProcess.stdout.on('data', (data) => {
-			console.log(`Python script output: ${data}`);
-		});
-		pythonProcess.stderr.on('data', (data) => {
-			console.error(`Error from Python script: ${data}`);
-		});
-		pythonProcess.on('close', (code) => {
-			console.log(`Python script exited with code ${code}`);
-		});
+			/* ********** */
+			/* DIFF FILES */
+			/* ********** */
 
-	} else if (answer == '2') {
+			console.log('🤖 Diff files:');
 
-		/* ****************** */
-		/* PUNTEN SHEET MAKEN */
-		/* ****************** */
+			const pythonProcess = spawn('/usr/bin/python3', ['batchFileDiff.py'], { cwd: 'diff-file/' });
+			// /usr/bin/python3: the location of the Python3 binary, find with this command $ which python3
+			// cwd: the location from where the python scripts is executed
+			pythonProcess.stdout.on('data', (data) => {
+				console.log(`Python script output: ${data}`);
+			});
+			pythonProcess.stderr.on('data', (data) => {
+				console.error(`Error from Python script: ${data}`);
+			});
+			pythonProcess.on('close', (code) => {
+				console.log(`Python script exited with code ${code}`);
+			});
 
-		console.log('🤖 Punten sheet maken:');
+		} else if (answer == '2') {
 
-		spawn('node punten/csv-to-sheet.js', (error, stdout, stderr) => {
-			if (error) {
-				console.error(`exec error: ${error}`);
-				return;
-			}
-			console.log(`stdout: ${stdout}`);
-			console.error(`stderr: ${stderr}`);
-		});
+			/* ****************** */
+			/* PUNTEN SHEET MAKEN */
+			/* ****************** */
+
+			console.log('🤖 Punten sheet maken:');
+
+			spawn('node punten/csv-to-sheet.js', (error, stdout, stderr) => {
+				if (error) {
+					console.error(`exec error: ${error}`);
+					return;
+				}
+				console.log(`stdout: ${stdout}`);
+				console.error(`stderr: ${stderr}`);
+			});
 
 
-	} else {
-		console.log('🤖 Nothing...');
-	}
-	
-	rl.close();
-});
+		} else {
+			console.log('🤖 Nothing...');
+		}
+		
+		rl.close();
+	});
+
+/* ****************************** */
+/* ON ERROR READING SETTINGS JSON */
+/* ****************************** */
+
+} catch (error) {
+	console.error('❌ Error reading or parsing settings-maesbot.json:', error);
+}
