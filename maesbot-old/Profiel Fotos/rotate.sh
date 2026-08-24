31f53946212e1ce9283c86c28d04f6af4eba2879
Hanne on NixOS <maes.hanne@gmail.com>
2026-08-24 16:33:38 +0200

    Push fast: 2026-08-24 16:33:38

--- maesbot-old/Profiel Fotos/rotate.sh
+++
@@ -1,4 +0,0 @@
-# Beide (jhead & find) nog niet gestest, maar zou moeten werken volgen claude
-shopt -s globstar nullglob nocaseglob
-jhead -autorot **/*.{jpg,jpeg,JPG,JPEG}
-find . -type f \( -iname "*.jpg" -o -iname "*.jpeg" \) -exec jhead -autorot {} +
